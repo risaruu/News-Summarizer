993995e2071bb3e8248b78b5dfc6e2a0ee132ccb
Damjan Petrovic <p.damjan7999@gmail.com>
2020-12-19 21:01:21 +0100

    added simple sentiment analysis through library and display

--- main.py
+++ main.py
@@ -17,3 +17,7 @@ print(f'Title: {article.title}')
 print(f'Authors: {article.authors}')
 print(f'Publication Date: {article.publish_date}')
 print(f'Summary: {article.summary}')
+
+analysis = TextBlob(article.text)
+print(analysis.polarity)
+print(f'Sentiment: {"positive" if analysis.polarity > 0 else "negative" if analysis.polarity < 0 else "neutral"}')
