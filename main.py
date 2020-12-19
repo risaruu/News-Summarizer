985242c337bb57fd7705c1460503b2a8000891fc
Damjan Petrovic <p.damjan7999@gmail.com>
2020-12-19 20:56:43 +0100

    displaying data of the article in the console

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import nltk
 from textblob import TextBlob
 from newspaper import Article
 
-url = 'https://kurier.at/politik/inland/lockdown-nummer-drei-das-zerrt-jetzt-echt-an-den-nerven/401135265'
+nltk.download('punkt')
+
+url = 'https://www.noen.at/in-ausland/oesterreich-grenzkontrollen-bis-10-jaenner-verstaerkt-arbeitsmarkt-bundesheer-epidemie-reisen-zusammenfassung-oesterreich-238488125'
 article = Article(url)
 
 article.download()
 article.parse()
 
 article.nlp()
+
+print(f'Title: {article.title}')
+print(f'Authors: {article.authors}')
+print(f'Publication Date: {article.publish_date}')
+print(f'Summary: {article.summary}')
