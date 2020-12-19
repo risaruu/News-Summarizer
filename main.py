23a986c8f37798cbad9ca7b835612c8433df0a0c
Damjan Petrovic <p.damjan7999@gmail.com>
2020-12-19 20:46:35 +0100

    Setting up imports and url

---
+++ main.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+import nltk
+from textblob import TextBlob
+from newspaper import article
+
+url = 'https://kurier.at/politik/inland/lockdown-nummer-drei-das-zerrt-jetzt-echt-an-den-nerven/401135265'
